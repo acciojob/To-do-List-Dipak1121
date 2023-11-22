7bb0283ba12ceb3bea6d985873e732db9d2dd877
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-11-22 18:27:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here
-
+let inputField = document.getElementById("newTodoInput");
+let btn = document.getElementById("addTodoBtn");
+let list = document.getElementById("todoList");
+btn.addEventListener("click", onClick);
+function onClick() {
+	if(inputField.value == ""){
+		return;
+	}
+	let task = document.createElement("li");
+	task.innerText = inputField.value;
+	list.appendChild(task);
+    inputField.value = "";
+}
